e1abc37c77d2751729aa53e14b92dec7c62815d1
nishanthkumarg <nishanthkumarg@google.com>
2016-08-31 15:55:14 -0700

    first commit

---
+++ app/.externalNativeBuild/ndkBuild/debug/obj/local/arm64-v8a/objs/apple/src/pie.o.d
@@ -0,0 +1,2 @@
+/Users/nishanthkumarg/Nishanth/2.2/145.3225885/Projects/probable-umbrella/app/.externalNativeBuild/ndkBuild/debug/obj/local/arm64-v8a/objs/apple/src/pie.o: \
+  /Users/nishanthkumarg/Nishanth/2.2/145.3225885/Projects/probable-umbrella/app/src/main/cpp/libapple/src/pie.c
---
+++ app/.externalNativeBuild/ndkBuild/debug/obj/local/arm64-v8a/objs/banana/src/pie.o.d
@@ -0,0 +1,2 @@
+/Users/nishanthkumarg/Nishanth/2.2/145.3225885/Projects/probable-umbrella/app/.externalNativeBuild/ndkBuild/debug/obj/local/arm64-v8a/objs/banana/src/pie.o: \
+  /Users/nishanthkumarg/Nishanth/2.2/145.3225885/Projects/probable-umbrella/app/src/main/cpp/libbanana/src/pie.c
